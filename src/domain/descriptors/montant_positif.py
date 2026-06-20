e5077bdd69af969f5272a9f086b9e7ad978c093c
Mountata <renelegrandmountata@gmail.com>
2026-06-20 20:06:39 +0000

    feat(descriptors): ajoute MontantPositif pour valider les montants positifs

---
+++ src/domain/descriptors/montant_positif.py
@@ -0,0 +1,51 @@
+class MontantPositif:
+
+    def __set_name__(self, owner, name):
+        # Appelé automatiquement par python quon on ecrit :
+        # montant = MontantPositif
+        # owner = classe (ex: Facture), name = "montant"
+
+        self.nom_prive = f"_{name}"
+
+    def __get__(self, instance, owner):
+        # instance = objet une facture precise
+        # owner la classe
+        if instance is None:
+            return self # acces via la classe pas l'instance 
+        
+        return getattr(instance, self.nom_prive, None)
+    
+    def __set__(self, instance, value):
+        
+        if not isinstance(value, (int, float)):
+            raise TypeError(f"Le montant doit etre un nombre : {type(value).__name__}")
+        if value <0 :
+            raise ValueError(f"le montant doit etre positif ou nul:, recu:{value}")
+        
+        setattr(instance, self.nom_prive, value)
+
+
+# ---------- VOS TESTS ----------
+if __name__ == "__main__":
+    class Test:
+        montant = MontantPositif()
+
+    t = Test()
+    t.montant = 100
+    print(f"✅ Valeur acceptée : {t.montant}")
+
+    print("Je vais maintenant tenter de mettre -50...")
+    try:
+        t.montant = -50
+    except ValueError as e:
+        print(f"❌ Erreur attrapée comme prévu : {e}")
+        
+            
+
+
+
+
+
+
+
+
