bdde58c6486aee6dc07fc94f658fc1918e34d2cc
Mountata <renelegrandmountata@gmail.com>
2026-06-21 18:25:59 +0000

    refactor(client): applique SerializableMixin et AuditableMixin à Client

--- src/domain/entities/client.py
+++ src/domain/entities/client.py
@@ -1,8 +1,12 @@
-class Client:
+from src.domain.mixins.serializable_mixin import SerializableMixin
+from src.domain.mixins.auditable_mixin import AuditableMixin
+
+class Client(SerializableMixin, AuditableMixin):
     def __init__(self, nom: str, email: str, siret: str):
         self.nom = nom
         self.email = email
         self.siret = siret
+        self._init_audit()
 
     def __repr__(self):
         return f"Client(nom={self.nom!r}, email={self.email!r})"
